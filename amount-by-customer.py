d643366df985b6f227643561e7f0f86b716e7ac9
ddutjnrevenge <hanhnguyen210704@gmail.com>
2024-06-30 10:11:33 +0700

    adding total_amount_by_customer example

---
+++ amount-by-customer.py
@@ -0,0 +1,21 @@
+# Find total amount spent by each customer
+from pyspark import SparkConf, SparkContext
+conf = SparkConf().setMaster("local").setAppName("AmountByCustomer")
+sc = SparkContext(conf = conf)
+
+def extractCustomerPrice(line):
+    fields = line.split(',')
+    customerID = int(fields[0])
+    amount = float(fields[2])
+    return (customerID, amount)
+
+input = sc.textFile("D:/Data_Engineering/Apache_Spark/customer-orders.csv")
+# map each line to a tuple of (customerID, amount)
+parsedLines = input.map(extractCustomerPrice)
+# add up the amounts for each customer
+totalByCustomer = parsedLines.reduceByKey(lambda x, y: x + y).sortByKey()
+# collect the results
+results = totalByCustomer.collect()
+# print the results
+for result in results:
+    print(result)
