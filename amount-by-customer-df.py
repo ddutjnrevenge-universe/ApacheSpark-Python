dc979486b45adb0899a2b4bbf60079a11603180e
ddutjnrevenge <hanhnguyen210704@gmail.com>
2024-06-30 14:01:41 +0700

    Calc amount spent by customer using DataFrame

---
+++ amount-by-customer-df.py
@@ -0,0 +1,21 @@
+from pyspark.sql import SparkSession
+from pyspark.sql import functions as func
+from pyspark.sql.types import StructType, StructField, StringType, IntegerType, FloatType
+spark = SparkSession.builder.appName("AmountByCustomer").getOrCreate()
+
+schema = StructType([StructField("customerID", IntegerType(), True),\
+                     StructField("itemID", IntegerType(), True),\
+                        StructField("amount", FloatType(), True)])
+
+df = spark.read.schema(schema).csv("D:/Data_Engineering/Apache_Spark/customer-orders.csv")
+df.printSchema()
+
+# Group by customerID and sum up the amount round to 2 decimal places
+totalByCustomer = df.groupBy("customerID").agg(func.round(func.sum("amount"),2).alias("totalAmount"))
+
+# Sort by total spent
+totalByCustomerSorted = totalByCustomer.sort("totalAmount")
+
+totalByCustomerSorted.show(totalByCustomerSorted.count())
+
+spark.stop()
