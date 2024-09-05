6f95947d4d37524bb2f126881d419a02236f5394
ddutjnrevenge-universe <hanhnguyen210704@gmail.com>
2024-09-05 14:34:59 +0700

    Azure Data Factory Pipeline test

---
+++ AdventureWorksLT/lookup_tables.sql
@@ -0,0 +1,11 @@
+SELECT 
+    s.name AS SchemaName,
+    t.name AS TableName
+FROM 
+    sys.tables t
+INNER JOIN 
+    sys.schemas s
+ON 
+    t.schema_id = s.schema_id
+WHERE 
+    s.name = 'SalesLT';
